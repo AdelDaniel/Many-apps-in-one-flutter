de07481ebc347116d85d335d540719ee8274e0af
adel_daniel <adel.daniel.222@gmail.com>
2021-11-19 22:40:33 +0200

    add internet_connection_checker package and test it

--- lib/core/platforms/network_info.dart
+++ lib/core/platforms/network_info.dart
@@ -1,3 +1,16 @@
+import 'dart:async';
+
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkInfo {
+  const NetworkInfo();
   Future<bool> get isConnected;
+}
+
+class NetWorkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker internetConnectionChecker;
+  const NetWorkInfoImpl(this.internetConnectionChecker);
+  
+  @override
+  Future<bool> get isConnected => internetConnectionChecker.hasConnection;
 }
---
+++ test/core/platforms/network_info_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:xylophoneflutter/core/platforms/network_info.dart';
+import 'network_info_test.mocks.dart';
+
+// class MockInternetConnectionChecker extends Mock
+//     implements InternetConnectionChecker {}
+
+@GenerateMocks([InternetConnectionChecker])
+void main() {
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
+  late NetWorkInfoImpl netWorkInfoImpl;
+  final tTrueConnectionFuture = Future.value(true);
+  final tFalseHasConnectionFuture = Future.value(false);
+
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    netWorkInfoImpl = NetWorkInfoImpl(mockInternetConnectionChecker);
+  });
+  test('should return true when there is internet connection ', () {
+    // arrange
+    when(mockInternetConnectionChecker.hasConnection)
+        .thenAnswer((_) => tTrueConnectionFuture);
+    // act
+    // NOTICE: We're NOT awaiting the result
+    final actualResult = netWorkInfoImpl.isConnected;
+    // assert
+    verify(mockInternetConnectionChecker.hasConnection);
+    // Utilizing Dart's default referential equality.
+    // Only references to the same object are equal.
+    expect(actualResult, equals(tTrueConnectionFuture));
+  });
+
+  test('should return False when No internet connection ', () {
+    // arrange
+    when(mockInternetConnectionChecker.hasConnection)
+        .thenAnswer((_) => tFalseHasConnectionFuture);
+    // act
+    // NOTICE: We're NOT awaiting the result
+    final actualResult = netWorkInfoImpl.isConnected;
+    // assert
+    verify(mockInternetConnectionChecker.hasConnection);
+    // Utilizing Dart's default referential equality.
+    // Only references to the same object are equal.
+    expect(actualResult, equals(tFalseHasConnectionFuture));
+  });
+}
